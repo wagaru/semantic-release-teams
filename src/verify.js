27a7ccd5b585dac96394896c14923574a218c78e
wagaru <wagaru@hotmail.com>
2020-10-21 17:36:14 +0800

    fix: 修正verify log

--- src/verify.js
+++ src/verify.js
@@ -4,13 +4,13 @@ module.exports = async(pluginConfig, context) => {
     const { logger, env: { WEBHOOK_URL } } = context;
     const errors = [];
 
-    logger.log('verify required webhookUrl')
     if (!WEBHOOK_URL) {
+        logger.log('verify required webhookUrl failed')
         errors.push(new Error('WEBHOOK_URL is required.'))
     }
 
     if (errors.length > 0) {
         throw new AggregateError(errors);
-        
+
     }
 }
